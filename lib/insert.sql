9d22e5f224a7aea28ae071e74dab4009bd045b66
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-01-28 23:57:31 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,4 +8,4 @@ INSERT INTO Books (title, year, series_id) VALUES ("book 4", 1941, 1);
 INSERT INTO Books (title, year, series_id) VALUES ("book 5", 1952, 1);
 INSERT INTO Books (title, year, series_id) VALUES ("book 6", 1972, 1);
 
-INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1, 1), (2, "Tyrion Lanister", "A Lanister always pays is debts", "human", 1, 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), (4, "Eddard Stark", "Winter is coming", "human", 1, 1);
+INSERT INTO Characters (id, name, motto, species, author_id, series_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1, 1), (2, "Tyrion Lanister", "A Lanister always pays is debts", "human", 1, 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), (4, "Eddard Stark", "Winter is coming", "human", 1, 1);
