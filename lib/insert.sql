828c4b967c9ff8772dea4311ab82f66a0ea3faaf
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-01-28 23:49:14 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,9 @@
 INSERT INTO Series (title, author_id, subgenre_id) VALUES ("series 1", 1, 1);
 INSERT INTO Series (title, author_id, subgenre_id) VALUES ("series 2", 2, 2);
 
+INSERT INTO Books (title, year, series_id) VALUES ("book 1", 1954, 2);
+INSERT INTO Books (title, year, series_id) VALUES ("book 2", 1954, 2);
+INSERT INTO Books (title, year, series_id) VALUES ("book 3", 1955, 2);
+INSERT INTO Books (title, year, series_id) VALUES ("book", 1997, 1);
+INSERT INTO Books (title, year, series_id) VALUES ("book", 1998, 1);
+INSERT INTO Books (title, year, series_id) VALUES ("Harry Potter and the Prisoner of Azkaban", 1999, 1);
