660a9ae549cd85bde1c1e7ffc976dc583e1508a7
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-01-28 23:56:12 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,7 +8,7 @@ INSERT INTO Books (title, year, series_id) VALUES ("book 4", 1941, 1);
 INSERT INTO Books (title, year, series_id) VALUES ("book 5", 1952, 1);
 INSERT INTO Books (title, year, series_id) VALUES ("book 6", 1972, 1);
 
-INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Galadriel", "This is Nenya, the Ring of Adamant, and I am its keeper.", "elf", 2, 2);
+INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1, 1);
 INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Legolas", "Do not think I won't kill you, dwarf! It would be my pleasure", "elf", 2, 2);
 INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Frodo Baggins", "I will take the Ring, though I do not know the way...", "hobbit", 2, 2);
 INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Harry Potter", "Expeliamus!", "human", 1, 1);
