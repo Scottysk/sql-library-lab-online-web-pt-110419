b69af0144f0443bb2b47e4e0949f2c95fc2f36b7
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-01-28 23:50:24 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,3 +7,12 @@ INSERT INTO Books (title, year, series_id) VALUES ("book 3", 1940, 2);
 INSERT INTO Books (title, year, series_id) VALUES ("book 4", 1941, 1);
 INSERT INTO Books (title, year, series_id) VALUES ("book 5", 1952, 1);
 INSERT INTO Books (title, year, series_id) VALUES ("book 6", 1972, 1);
+
+INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("moo", "nyeeh.", "meow", 2, 2);
+INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Legolas", "Do not think I won't kill you, dwarf! It would be my pleasure", "elf", 2, 2);
+INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Frodo Baggins", "I will take the Ring, though I do not know the way...", "hobbit", 2, 2);
+INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Harry Potter", "Expeliamus!", "human", 1, 1);
+INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Hermione Granger", "You're going to get yourself killed...", "human", 1, 1);
+INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Ron Weasley", "I've got a bad feeling about this...", "human", 1, 1);
+INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Lucius Malfoy", "Die Potter!", "human", 1, 1);
+INSERT INTO Characters (name, motto, species, author_id, series_id) VALUES ("Rubeus Hagrid", "Just gotta know how to calm 'em", "half-giant", 1, 1);
